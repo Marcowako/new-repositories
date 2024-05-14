5be749f5eadc34feb3b46bd87bf697c529d77e76
Marcos Msiska <marcowako@gmail.com>
2024-05-13 23:11:12 -0400

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -18,3 +18,26 @@ for (let skill of skills) {
     skillItem.innerText = skill;
     skillsList[0].appendChild(skillItem);
 }
+
+// Getting DOM elements
+const projectsSection = document.querySelector("#projects");
+const projectsList = projectsSection.querySelector("ul");
+fetch("https://api.github.com/users/Marcowako/repos")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("wrong parking, try again");
+    }
+    return res.json(); // parse the response as JSON
+  })
+  .then((data) => {
+    for (let i = 0; i < data.length; i++) {
+      const project = document.createElement("li");
+      project. innerText = data[i].name;
+      projectsList.appendChild(project);
+    }
+  })
+  .catch((error) => {
+    const errorElement = document.createElement("p");
+    errorElement.innerText = error.message;
+    projectSection.appendChild(errorElement);
+  });
